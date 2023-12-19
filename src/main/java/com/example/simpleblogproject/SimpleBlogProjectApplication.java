90dd1ca5eb5d73e3a0948915e3674c37a48e42a3
Kim-Jong-Gyu <whdrb2643@gmail.com>
2023-12-19 19:29:33 +0900

    first commit

---
+++ src/main/java/com/example/simpleblogproject/SimpleBlogProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleblogproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlogProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBlogProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simpleblogproject/SimpleBlogProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleblogproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlogProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
