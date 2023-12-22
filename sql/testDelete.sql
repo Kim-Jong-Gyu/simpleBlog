70922741fd19f9bee7865e4b44ec29803dcca6da
Kim-Jong-Gyu <whdrb2643@gmail.com>
2023-12-22 14:50:38 +0900

    feat : 스케줄러를 이용한 게시글 삭제 기능 추가

---
+++ sql/testDelete.sql
@@ -0,0 +1,37 @@
+insert into posts values (
+                          100,
+                          'test1',
+                          0,
+                          '2023-08-22 11:09:36.121394',
+                          '2023-08-22 11:09:36.121394',
+                          null, null,
+                          'test1',
+                          null );
+
+insert into posts values (
+                             101,
+                             'test2',
+                             0,
+                             '2023-08-22 11:09:36.121394',
+                             '2023-08-22 11:09:36.121394',
+                             null, null,
+                             'test2',
+                             null );
+insert into posts values (
+                             102,
+                             'test3',
+                             0,
+                             '2023-08-22 11:09:36.121394',
+                             '2023-08-22 11:09:36.121394',
+                             null, null,
+                             'test3',
+                             null );
+insert into posts values (
+                             103,
+                             'test4',
+                             0,
+                             '2023-08-22 11:09:36.121394',
+                             '2023-08-22 11:09:36.121394',
+                             null, null,
+                             'test4',
+                             null );
